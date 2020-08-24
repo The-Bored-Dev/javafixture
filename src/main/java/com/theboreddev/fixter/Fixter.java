190c50ef4718e0a4ac346eefe76e15d3936b4c38
theboreddev <account@theboreddev.com>
2020-08-24 17:56:34 +0200

    Provide method to generate a single object

--- src/main/java/com/theboreddev/fixter/Fixter.java
+++ src/main/java/com/theboreddev/fixter/Fixter.java
@@ -8,37 +8,75 @@ import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
-public class Fixter {
-    private final int size;
-    private final Class<?> type;
-    private Supplier<?> supplier;
-    private final Map<String, Supplier<?>> fieldSuppliers = new HashMap<>();
+public abstract class Fixter {
+    protected Class<?> type;
+    protected Supplier<?> supplier;
+    protected final Map<String, Supplier<?>> fieldSuppliers = new HashMap<>();
 
-    private Fixter(int size, Class<?> type) {
-        this.size = size;
-        this.type = type;
-    }
+    static class FixterCollectionsProvider extends Fixter {
+        private final int size;
 
-    public static Fixter of(int size, Class<?> type) {
-        return new Fixter(size, type);
-    }
-
-    public <T> Fixter withSupplier(Supplier<T> supplier) {
-        this.supplier = supplier;
-        return this;
-    }
-
-    public <U> Fixter withFieldSupplier(String fieldName, Supplier<U> supplier) {
-        if (type.isPrimitive()) {
-            throw new IllegalArgumentException("Cannot set field supplier for a simple type!");
+        private FixterCollectionsProvider(int size, Class<?> type) {
+            this.size = size;
+            this.type = type;
         }
-        this.fieldSuppliers.put(fieldName, supplier);
-        return this;
+
+        protected <T> FixterCollectionsProvider withSupplier(Supplier<T> supplier) {
+            this.supplier = supplier;
+            return this;
+        }
+
+        protected <U> FixterCollectionsProvider withFieldSupplier(String fieldName, Supplier<U> supplier) {
+            if (type.isPrimitive()) {
+                throw new IllegalArgumentException("Cannot set field supplier for a simple type!");
+            }
+            this.fieldSuppliers.put(fieldName, supplier);
+            return this;
+        }
+
+        public <T> List<T> apply() {
+            return (List<T>) Stream.generate(() ->
+                    supplier != null ?
+                            new ElementSupplier(supplier, type).supplyElement() :
+                            new ElementSupplier<>(fieldSuppliers, type).supplyElement())
+                    .limit(size)
+                    .collect(toList());
+        }
     }
 
-    public <T> List<T> apply() {
-        return (List<T>) Stream.generate(() -> supplier != null ? new ElementSupplier(supplier, type).supplyElement() : new ElementSupplier<>(fieldSuppliers, type).supplyElement())
-                .limit(size)
-                .collect(toList());
+    static class FixterObjectProvider extends Fixter {
+
+        private FixterObjectProvider(Class<?> type) {
+            this.type = type;
+        }
+
+        protected <T> FixterObjectProvider withSupplier(Supplier<T> supplier) {
+            this.supplier = supplier;
+            return this;
+        }
+
+        protected <U> FixterObjectProvider withFieldSupplier(String fieldName, Supplier<U> supplier) {
+            if (type.isPrimitive()) {
+                throw new IllegalArgumentException("Cannot set field supplier for a simple type!");
+            }
+            this.fieldSuppliers.put(fieldName, supplier);
+            return this;
+        }
+
+        public <T> T apply() {
+            return (T) (supplier != null ?
+                                        new ElementSupplier(supplier, type).supplyElement() :
+                                        new ElementSupplier<>(fieldSuppliers, type).supplyElement());
+        }
+    }
+
+
+
+    public static FixterCollectionsProvider of(int size, Class<?> type) {
+        return new FixterCollectionsProvider(size, type);
+    }
+
+    public static FixterObjectProvider of(Class<?> type) {
+        return new FixterObjectProvider(type);
     }
 }
